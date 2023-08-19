05cea847a2033b671c7c7f15881f31e7b8d2cbf2
ben shaya <bensh240@gmail.com>
2023-08-19 14:55:33 +0300

    Changing the visibility of the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,89 +1,66 @@
-import React, { Component } from 'react';
-import '../styles/Footer.css';
-import { Box, Typography, IconButton } from '@mui/material';
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Box, Grid } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
-class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                <Box display="flex" justifyContent="center" alignItems="center" flexDirection="row" id="Contact Me">
-                    {/* Ben Shaya's Social Links */}
-                    <Box border={1} borderRadius={8} padding={2} display="flex" flexDirection="column" alignItems="center" marginRight={3}>
-                        <Box display="flex" justifyContent="center">
-                            <IconButton
-                                href="https://www.facebook.com/ben.shaya.5"
-                                target="_blank"
-                                rel="noreferrer"
-                                color="primary"
-                                aria-label="Facebook"
-                            >
-                                <FacebookIcon />
-                            </IconButton>
-                            <IconButton
-                                href="https://il.linkedin.com/in/ben-shaya-939948202?trk=people-guest_people_search-card"
-                                target="_blank"
-                                rel="noreferrer"
-                                color="primary"
-                                aria-label="LinkedIn"
-                            >
-                                <LinkedInIcon />
-                            </IconButton>
-                            <IconButton
-                                href="https://www.instagram.com/ben_shaya/"
-                                target="_blank"
-                                rel="noreferrer"
-                                color="primary"
-                                aria-label="Instagram"
-                            >
-                                <InstagramIcon />
-                            </IconButton>
+const Footer = () => {
+    return (
+        <AppBar position="fixed" color="default" sx={{
+            backgroundColor: '#e0e0e0',
+            padding: '5px 0',
+            marginTop: '10px',
+            fontFamily: "'Roboto Condensed', sans-serif",
+            bottom: 0,
+            width: '100%',
+            flexGrow: 0,
+            top: 'auto'
+        }}>
+            <Toolbar>
+                <Grid container spacing={3} alignItems="center">
+                    <Grid item xs={6} sm={6} md={4}>
+                        <Box textAlign="center">
+                            <Box display="flex" justifyContent="center" gap={2}>
+                                <IconButton href="https://www.facebook.com/ben.shaya.5" target="_blank" rel="noreferrer" sx={{ '&:hover': { color: '#1a237e' } }}>
+                                    <FacebookIcon />
+                                </IconButton>
+                                <IconButton href="https://il.linkedin.com/in/ben-shaya-939948202?trk=people-guest_people_search-card" target="_blank" rel="noreferrer" sx={{ '&:hover': { color: '#1a237e' } }}>
+                                    <LinkedInIcon />
+                                </IconButton>
+                                <IconButton href="https://www.instagram.com/ben_shaya/" target="_blank" rel="noreferrer" sx={{ '&:hover': { color: '#1a237e' } }}>
+                                    <InstagramIcon />
+                                </IconButton>
+                            </Box>
+                            <Typography variant="body1">Ben Shaya</Typography>
                         </Box>
-                        <Typography variant="body1">Ben Shaya</Typography>
-                    </Box>
+                    </Grid>
 
-                    {/* Aviels Nisanov's Social Links */}
-                    <Box border={1} borderRadius={8} padding={2} display="flex" flexDirection="column" alignItems="center">
-                        <Box display="flex" justifyContent="center">
-                            <IconButton
-                                href="https://www.facebook.com/avielni"
-                                target="_blank"
-                                rel="noreferrer"
-                                color="primary"
-                                aria-label="Facebook"
-                            >
-                                <FacebookIcon />
-                            </IconButton>
-                            <IconButton
-                                href="https://www.linkedin.com/in/aviel-nisanov-84b407203/"
-                                target="_blank"
-                                rel="noreferrer"
-                                color="primary"
-                                aria-label="LinkedIn"
-                            >
-                                <LinkedInIcon />
-                            </IconButton>
-                            <IconButton
-                                href="https://www.instagram.com/avielnis/"
-                                target="_blank"
-                                rel="noreferrer"
-                                color="primary"
-                                aria-label="Instagram"
-                            >
-                                <InstagramIcon />
-                            </IconButton>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <Typography align="center" variant="body2">
+                            © Copyright Ben Shaya & Aviels Nisanov 2023. All Rights Reserved.
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={6} sm={6} md={4}>
+                        <Box textAlign="center">
+                            <Box display="flex" justifyContent="center" gap={2}>
+                                <IconButton href="https://www.facebook.com/avielni" target="_blank" rel="noreferrer" sx={{ '&:hover': { color: '#1a237e' } }}>
+                                    <FacebookIcon />
+                                </IconButton>
+                                <IconButton href="https://www.linkedin.com/in/aviel-nisanov-84b407203/" target="_blank" rel="noreferrer" sx={{ '&:hover': { color: '#1a237e' } }}>
+                                    <LinkedInIcon />
+                                </IconButton>
+                                <IconButton href="https://www.instagram.com/avielnis/" target="_blank" rel="noreferrer" sx={{ '&:hover': { color: '#1a237e' } }}>
+                                    <InstagramIcon />
+                                </IconButton>
+                            </Box>
+                            <Typography variant="body1">Aviels Nisanov</Typography>
                         </Box>
-                        <Typography variant="body1">Aviels Nisanov</Typography>
-                    </Box>
-                </Box>
-                <Typography align="center">
-                    © Copyright Ben Shaya & Aviels Nisanov 2023. All Rights Reserved.
-                </Typography>
-            </footer>
-        );
-    }
-}
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default Footer;
